8098cf69de6079814f852f270fccf67fe1722770
Skander Gharbi <123510581+skander2001@users.noreply.github.com>
2024-10-26 10:15:38 +0200

    Backend : CRUD

--- backend/index.js
+++ backend/index.js
@@ -53,6 +53,65 @@ app.get("/books", async (request, response) => {
   }
 });
 
+// Route for Get One book by id
+app.get("/books/:id", async (request, response) => {
+  try {
+    const { id } = request.params;
+    const books = await Book.findById(id);
+
+    return response.status(200).json({
+      count: books.length,
+      data: books,
+    });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+// Route for Update a book
+app.put("/books/:id", async (request, response) => {
+  try {
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publishYear
+    ) {
+      response.status(400).send({
+        message: "Send all required fields : title , author , publishYear",
+      });
+    }
+    const { id } = request.params;
+    const result = await Book.findByIdAndUpdate(id, request.body);
+    if (!result) {
+      return response.status(404).json({ message: "Book not found!" });
+    }
+    return response
+      .status(200)
+      .json({ message: "Book updated successfully!!" });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+// Route for Delete a Book
+app.delete("/books /:id", async (request, response) => {
+  try {
+    const { id } = request.params;
+    const result = Book.findByIdAndDelete(id);
+    if (!result) {
+      return response.status(404).json({ message: "Book not found!" });
+    }
+    return response
+      .status(200)
+      .send({ message: "Book deleted successfully!!" });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
 mongoose
   .connect(mongoDBURL)
   .then(() => {
