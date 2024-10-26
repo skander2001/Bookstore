ad8c47a9d3ae783d01b5980cb4a24056485d2936
Skander Gharbi <123510581+skander2001@users.noreply.github.com>
2024-10-26 09:25:38 +0200

    Backend : Get All / Add new book

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,6 @@
-export const PORT = 5555;
-export const mongoDBURL =
-  "mongodb+srv://root:root@book-store-mern.qiwd9.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book-store-MERN";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const PORT = process.env.PORT || 5555;
+export const mongoDBURL = process.env.MONGODB_URL;
--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,56 @@
-import express from "express";
+import express, { request, response } from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
 
 const app = express();
+
+// Middleware for parsing request body
+app.use(express.json());
 
 app.get("/", (request, response) => {
   console.log(request);
   return response.status(234).send("welcome");
+});
+
+// Route to add a new book
+app.post("/books", async (request, response) => {
+  try {
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publishYear
+    ) {
+      response.status(400).send({
+        message: "Send all required fields : title , author , publishYear",
+      });
+    }
+    const newbook = {
+      title: request.body.title,
+      author: request.body.author,
+      publishYear: request.body.publishYear,
+    };
+    const book = await Book.create(newbook);
+    return response.status(201).send(book);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+// Route for Get All Books from database
+app.get("/books", async (request, response) => {
+  try {
+    const books = await Book.find({});
+
+    return response.status(200).json({
+      count: books.length,
+      data: books,
+    });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
 });
 
 mongoose
