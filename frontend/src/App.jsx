ccaa7be432341a83f6f0a0d1418b306f08dfd97d
Skander Gharbi <123510581+skander2001@users.noreply.github.com>
2024-10-28 14:55:27 +0100

    React Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import {Routes,Route} from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import CreateBooks from './pages/CreateBooks.jsx'
+import DeleteBooks from './pages/DeleteBooks.jsx'
+import EditBooks from './pages/EditBooks.jsx'
+import ShowBook from './pages/ShowBook.jsx'
+
 
 const App = () => {
   return (
-    <div className='bg-red-400 text-white'>App</div>
+    <Routes>
+      <Route path='/'element={Home}></Route>
+      <Route path='/books/create'element={CreateBooks}></Route>
+      <Route path='/books/delete/:id'element={DeleteBooks}></Route>
+      <Route path='/books/edit/:id'element={EditBooks}></Route>
+      <Route path='/books/details/:id 'element={ShowBook}></Route>
+    </Routes>
   )
 }
 
