2046d80b95fa6d081db14a0ae37dd330b5a833e9
Skander <skandergharbi.contact@gmail.com>
2024-12-13 18:22:20 +0100

    Home

--- backend/config.js
+++ backend/config.js
@@ -2,5 +2,5 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const PORT = process.env.PORT || 5555;
-export const mongoDBURL = process.env.MONGODB_URL;
+export const PORT = 5555;
+export const mongoDBURL = 'mongodb+srv://root:root@book-store-mern.qiwd9.mongodb.net/?retryWrites=true&w=majority&appName=Book-store-MERN';
